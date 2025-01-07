eeb48407f86e5242622bdff2de0c7b6cdc162a84
M-Soriano <104050536+M-Soriano@users.noreply.github.com>
2025-01-06 21:10:23 -0800

    updated crowdfunding_db_schema.sql with comments on top

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,6 +1,6 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
+-- Added to modificaton in postgres for campaign table other work fine.
 
 CREATE TABLE "campaign" (
     "cf_id" INTEGER   NOT NULL,
